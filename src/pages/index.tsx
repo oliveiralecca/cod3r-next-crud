dba487b3c49b050c7ff53ad287a7b37269f962cd
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-11-17 01:18:10 -0300

    layout, config, client class

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
+  )
+}
